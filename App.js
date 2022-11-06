4a4fbe5279b099fabbe0f49de5ad43a0092f994a
NachoConort <nachoconort@gmail.com>
2022-11-06 14:26:22 -0300

    Fixed error

--- App.js
+++ App.js
@@ -2,5 +2,5 @@ import { View } from 'react-native';
 import PracticaAsyncStorage from './src/components/PracticaAsyncStorage';
 
 export default function App() {
-  return <PracticaAsyncStorage/>
-}
+  return <View></View>
+};
