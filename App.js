9bf3b36daa5494b5f323d2dc4c9262838e363dee
NachoConort <nachoconort@gmail.com>
2022-11-06 13:20:37 -0300

    Preparing work area

--- App.js
+++ App.js
@@ -1,17 +1,8 @@
-import { StyleSheet, View } from 'react-native';
+import { View } from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
+    <View>
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
